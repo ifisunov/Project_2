fa046dc81c65a2777443b8a3421771a9a3cb2852
Ivan <ivfisunov@gmail.com>
2019-01-05 21:35:49 +0300

    7 task

--- src/renderers/renderselect.js
+++ src/renderers/renderselect.js
@@ -4,6 +4,7 @@ import plainRenderAST from './renderplain';
 const selectRenderType = {
   ast: renderAST,
   plain: plainRenderAST,
+  json: JSON.stringify,
 };
 
 export default (ast, type) => selectRenderType[type](ast);
